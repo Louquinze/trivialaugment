7104cbf205564c34116119b7eb988949331b3dae
lukas <lukas.j.m.strack@gmail.com>
2022-12-02 16:20:10 +0100

    init

---
+++ create_new_yaml.py
@@ -0,0 +1,15 @@
+import yaml
+from yaml.loader import SafeLoader
+import os
+
+if __name__ == '__main__':
+    for file in os.listdir("confs"):
+        # print(file)
+        # Open the file and load the file
+        with open(f"confs/{file}") as f:
+            data = yaml.load(f, Loader=SafeLoader)
+            print(data)
+
+        with open(f"confs/{file.replace('128', '64')}", 'w') as f:
+            data["batch"] = 64
+            data = yaml.dump(data, f, sort_keys=False, default_flow_style=False)
