3eefe0d371d7c9b67d6e99938f11c2a13e5641c8
lukas <lukas.j.m.strack@gmail.com>
2023-01-13 13:45:03 +0100

    prep final eval wresnet

--- create_run.py
+++ create_run.py
@@ -11,7 +11,7 @@ with open("run.txt", "w") as f:
     for seed in range(5):
         for func in func_lst:
             f.write(
-                f"python -m TrivialAugment.train -c confs/wresnet28x10_cifar10_b64_maxlr.1_randaugn=1,m=30_fixedsesp_nowarmup_200epochs.yaml.yaml"
+                f"python -m TrivialAugment.train -c confs/wresnet28x10_cifar10_b64_maxlr.1_randaugn=1,m=30_fixedsesp_nowarmup_200epochs.yaml"
                 f" --dataroot data --tag w28x10_EXPERIMENT_cifar10_{func}_{seed} --seed {seed} --func {func}"
                 f"\n")
 
