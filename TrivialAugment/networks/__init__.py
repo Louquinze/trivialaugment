9ad160d61575888d96906f955300326f1734d562
lukas <lukas.j.m.strack@gmail.com>
2023-05-24 14:50:03 +0900

    add new models and disalbe not supported ones

--- TrivialAugment/networks/__init__.py
+++ TrivialAugment/networks/__init__.py
@@ -24,11 +24,21 @@ def get_model(conf, bs, activation, num_class=10, writer=None):
         activation = nn.ReLU
     elif activation == "silu":
         activation = nn.SiLU
+    elif activation == "elu":
+        activation = nn.ELU
+    elif activation == "leakyrelu":
+        activation = nn.LeakyReLU
+    elif activation == "gelu":
+        activation = nn.GELU
     else:
         raise KeyError
 
-    if name == 'resnet18':
+    if name == 'resnet18cifar10':
         model = ResNet(dataset='cifar10', depth=18, num_classes=num_class, bottleneck=True, activation=activation)
+    elif name == 'resnet34cifar10':
+        model = ResNet(dataset='cifar10', depth=34, num_classes=num_class, bottleneck=True, activation=activation)
+    elif name == 'resnet50cifar10':
+        model = ResNet(dataset='cifar10', depth=50, num_classes=num_class, bottleneck=True, activation=activation)
     elif name == 'resnet50':
         model = ResNet(dataset='imagenet', depth=50, num_classes=num_class, bottleneck=True, activation=activation)
     elif name == 'resnet200':
@@ -42,16 +52,16 @@ def get_model(conf, bs, activation, num_class=10, writer=None):
                            adaptive_dropouter_creator=ad_creators[0], adaptive_conv_dropouter_creator=ad_creators[1],
                            groupnorm=conf.get('groupnorm', False), examplewise_bn=conf.get('examplewise_bn', False),
                            virtual_bn=conf.get('virtual_bn', False), activation=activation)
-    elif name == 'miniconvnet':
-        model = SeqConvNet(num_class,adaptive_dropout_creator=ad_creators[0],batch_norm=False, activation=activation)
-    elif name == 'mlp':
-        model = MLP(num_class, (3,32,32), adaptive_dropouter_creator=ad_creators[0], activation=activation)
-    elif name == 'shakeshake26_2x96d':
-        model = ShakeResNet(26, 96, num_class, activation=activation)
-    elif name == 'shakeshake26_2x112d':
-        model = ShakeResNet(26, 112, num_class, activation=activation)
-    elif name == 'shakeshake26_2x96d_next':
-        model = ShakeResNeXt(26, 96, 4, num_class, activation=activation)
+    # elif name == 'miniconvnet':
+    #     model = SeqConvNet(num_class,adaptive_dropout_creator=ad_creators[0],batch_norm=False, activation=activation)
+    # elif name == 'mlp':
+    #     model = MLP(num_class, (3,32,32), adaptive_dropouter_creator=ad_creators[0], activation=activation)
+    # elif name == 'shakeshake26_2x96d':
+    #     model = ShakeResNet(26, 96, num_class, activation=activation)
+    # elif name == 'shakeshake26_2x112d':
+    #     model = ShakeResNet(26, 112, num_class, activation=activation)
+    # elif name == 'shakeshake26_2x96d_next':
+    #     model = ShakeResNeXt(26, 96, 4, num_class, activation=activation)
     else:
         raise NameError('no model named, %s' % name)
 
@@ -62,8 +72,6 @@ def get_model(conf, bs, activation, num_class=10, writer=None):
     #model = model.cuda()
     #model = DataParallel(model)
     cudnn.benchmark = True
-    print(model)
-    raise NotImplementedError
     return model
 
 
