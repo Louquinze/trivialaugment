da927df8d8895a79586170de80ed4a6ca84537be
lukas <lukas.j.m.strack@gmail.com>
2023-06-13 18:53:17 +0900

    prepare eval

---
+++ save_to_csv.py
@@ -0,0 +1,28 @@
+import os
+
+import pandas as pd
+import torch
+
+path = "save"
+res = None
+
+for file in os.listdir(path):
+    data = torch.load(path + "/" + file)
+    # func, network
+    d = {
+        "network": [file.split("_")[2]],
+        "func": [file.split("_")[1]],
+        "seed": [file.split("_")[0]],
+        "epoch": [data["epoch"]],
+        "test_top1": [data["log"]["test"]["top1"]],
+        "eval_test_top1": [data["log"]["test"]["eval_top1"]],
+    }
+
+    d = pd.DataFrame(d)
+
+    if res is None:
+        res = d
+    else:
+        res = pd.concat((res, d), ignore_index=True)
+
+    res.to_csv(path + ".csv")
